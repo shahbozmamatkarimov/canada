1673a1c6f6959a1eb5a6ad626cc2d481cb7f9207
shahbozhoca <shahboz@hoca.dev>
2023-12-14 13:41:20 +0300

    register page

--- pages/register/index.vue
+++ pages/register/index.vue
@@ -5,27 +5,27 @@
     >
       <ul class="flex justify-between items-center h-10">
         <li
-          @click="store.loginType = 'Customer'"
+          @click="store.registerType = 'Customer'"
           :class="
-            store.loginType != 'Business' ? 'bg-[#F24822] text-white' : ''
+            store.registerType != 'Business' ? 'bg-[#F24822] text-white' : ''
           "
           class="flex items-center cursor-pointer duration-500 justify-center h-full w-full rounded-[10px]"
         >
-          Customer login
+          Customer register
         </li>
         <li
-          @click="store.loginType = 'Business'"
+          @click="store.registerType = 'Business'"
           :class="
-            store.loginType == 'Business' ? 'bg-[#F24822] text-white' : ''
+            store.registerType == 'Business' ? 'bg-[#F24822] text-white' : ''
           "
           class="flex items-center cursor-pointer duration-500 justify-center h-full w-full rounded-[10px]"
         >
-          Business login
+          Business register
         </li>
       </ul>
       <form class="md:p-16 sm:p-10 p-5 py-16">
         <!-- <h1 class="text-2xl font-bold">Your logo</h1> -->
-        <!-- <h1 class="text-4xl font-bold mt-[13px] mb-[26px]">Login</h1> -->
+        <!-- <h1 class="text-4xl font-bold mt-[13px] mb-[26px]">register</h1> -->
 
         <div class="space-y-[30px]">
           <div class="space-y-[5px]">
@@ -65,14 +65,11 @@
             </div>
           </div>
         </div>
-        <router-link to="/login" class="block pt-2 pb-[30px]"
-          >Forgot Password?</router-link
-        >
         <button
-          class="flex items-center justify-center h-[50px] bg-[#F24822] rounded-[10px] w-full text-white text-xl font-bold"
+          class="flex mt-[30px] items-center justify-center h-[50px] bg-[#F24822] rounded-[10px] w-full text-white text-xl font-bold"
           type="submit"
         >
-          Sign in
+          Sign up
         </button>
         <p class="text-sm text-center my-8">or continue with</p>
         <div class="flex gap-[25px]">
@@ -93,9 +90,9 @@
           </div>
         </div>
         <p class="mt-8 text-center text-sm text-gray-800">
-          Don’t have an account yet?
+            Did you have already account?
           <router-link class="font-semibold text-black" to="/register"
-            >Register here</router-link
+            >Login here</router-link
           >
         </p>
       </form>
@@ -105,7 +102,7 @@
 
 <script setup>
 definePageMeta({
-  loginType: "Business",
+  registerType: "Business",
   layout: false,
 });
 
